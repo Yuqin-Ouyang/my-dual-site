04ddf61a2bc76ab2677976bb0a5a9ccddb00c2f6
Yuqin-Ouyang <136488832+Yuqin-Ouyang@users.noreply.github.com>
2025-05-27 00:52:37 +0800

    Delete js directory

--- js/redirect.js
+++
@@ -1,7 +0,0 @@
-fetch("https://ipapi.co/json")
-  .then(r => r.json())
-  .then(data => {
-    if (data && data.country_code === "CN") {
-      window.location.href = "https://cdn.jsdelivr.net/gh/Yuqin-Ouyang/my-dual-site@main/";
-    }
-  });
